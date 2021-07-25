17ba13e9b3b04854ba92d06f354f19646f2c918a
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-25 09:44:20 -0300

    Criado model de Product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+    has_many :products, dependent: :restrict_with_error
+
     validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_114009) do
+ActiveRecord::Schema.define(version: 2021_07_25_115629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,4 +46,15 @@ ActiveRecord::Schema.define(version: 2021_07_25_114009) do
     t.index ["uid", "provider"], name: "index_employes_on_uid_and_provider", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 10, scale: 2
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
