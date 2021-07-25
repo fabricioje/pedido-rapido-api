5c68497503c4ea3fc2b465932b4230ae491bcc76
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-25 08:53:09 -0300

    Criado model de Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_232525) do
+ActiveRecord::Schema.define(version: 2021_07_25_114009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "employes", force: :cascade do |t|
     t.string "provider", default: "email", null: false
